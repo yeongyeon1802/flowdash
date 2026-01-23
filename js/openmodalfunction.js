b393e7b0c4078063cc748df5e06af2c57978bf12
“ParkJunHyeon1209” <pjh140123@gmail.com>
2026-01-23 23:20:44 +0900

    feature/savebtnfunction and feature/openmodalfunction

---
+++ js/openmodalfunction.js
@@ -0,0 +1,30 @@
+const todolist = document.querySelectorAll(".element-list");
+const createbtn = document.querySelector(".add-todo-button");
+const maintitle = document.querySelector(".popup-main-title");
+
+function getTodos() {
+  const todo = localStorage.getItem("flowdash.todos");
+  return todo ? JSON.parse(todo) : [];
+}
+
+function clickNewBtn() {
+  dimmed.classList.toggle("hidden");
+  maintitle.textContent = "새 할 일";
+  console.log("openmodal");
+}
+
+function clickTodoList() {
+  const todos = getTodos();
+  dimmed.classList.toggle("hidden");
+  maintitle.textContent = "할 일 수정";
+  console.log("openmodal");
+}
+
+todolist.forEach((n) =>
+  n.addEventListener("click", function (e) {
+    const card = e.target.closest("li");
+    if (card) clickTodoList();
+  }),
+);
+
+createbtn.addEventListener("click", clickNewBtn);
---
+++ js/savebtnfunction.js
@@ -0,0 +1,50 @@
+const savebtn = document.querySelector(".popup-save-button");
+const title = document.querySelector("#popup-todo-title");
+const content = document.querySelector("#popup-todo-description");
+const priority = document.querySelector("input[name='priority']:checked");
+const statusvalue = document.querySelector("input[name='status']:checked");
+const dimmed = document.querySelector(".popup-modal");
+let count = 1;
+
+function getTodos() {
+  const todo = localStorage.getItem("flowdash.todos");
+  return todo ? JSON.parse(todo) : [];
+}
+
+function setTodos(todos) {
+  return localStorage.setItem("flowdash.todos", JSON.stringify(todos));
+}
+
+function rendor() {
+  const todos = getTodos();
+}
+
+function saveTodo() {
+  const todos = getTodos();
+
+  if (!title.value.trim()) {
+    const tooltip = document.createElement("p");
+    tooltip.className = "tooltip";
+    return;
+  }
+
+  const todo = {
+    id: count,
+    title: title.value.trim(),
+    content: content.value.trim(),
+    priority: priority.value,
+    statusvalue: statusvalue.value,
+    createAt: 123,
+    updateAt: 123,
+    completeAt: 123,
+  };
+
+  todos.push(todo);
+  setTodos(todos);
+  count++;
+
+  dimmed.classList.toggle("hidden");
+  rendor();
+}
+
+savebtn.addEventListener("click", saveTodo);
