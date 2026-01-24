3553b0a7bfc5c93a2bb971a28bd331478dbb0653
“ParkJunHyeon1209” <pjh140123@gmail.com>
2026-01-25 00:51:30 +0900

    feature/createfilterbadge
    뱃지 만드는 작업을 하려했지만 나도 모르게 리스트 수정할떄 데이터 불러오는 로직 완성 아직 수정할 때 로직은 구현해야함!

--- js/openmodalfunction.js
+++ js/openmodalfunction.js
@@ -5,17 +5,36 @@ function clickNewBtn() {
   title.focus();
 }
 
-function clickTodoList() {
+function clickTodoList(id) {
   const title = document.querySelector("#popup-todo-title");
   const todos = getStorage(FLOWDASH_TODOS);
+  const list = todos.find((todo) => todo.id === +id);
+  const content = document.querySelector("#popup-todo-description");
+  const high = document.querySelector("#high");
+  const medium = document.querySelector("#medium");
+  const low = document.querySelector("#low");
+  const todo = document.querySelector("#todo");
+  const doing = document.querySelector("#doing");
+  const done = document.querySelector("#done");
+
+  if (list.priority === "high") high.checked = true;
+  else if (list.priority === "medium") medium.checked = true;
+  else low.checked = true;
+
+  if (list.statusvalue === "todo") todo.checked = true;
+  else if (list.statusvalue === "doing") doing.checked = true;
+  else done.checked = true;
+
   dimmed.classList.toggle("hidden");
   maintitle.textContent = "할 일 수정";
+  title.value = list.title;
+  content.value = list.content;
 }
 
 todolist.forEach((n) =>
   n.addEventListener("click", function (e) {
     const card = e.target.closest("li");
-    if (card) clickTodoList();
+    if (card) clickTodoList(card.dataset.id);
   }),
 );
 
