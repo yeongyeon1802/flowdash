67a5a5ef6a726b7fbfb0941d85323cd6cd403c30
“ParkJunHyeon1209” <pjh140123@gmail.com>
2026-01-26 09:41:25 +0900

    feat/filter 진행 중 풀하기 위한 커밋

--- js/createFilterBadge.js
+++ js/createFilterBadge.js
@@ -10,7 +10,7 @@ function createFilterBadge() {
   const sortText = document.createElement("p");
   const sorttext = document.createElement("span");
   sortText.textContent = "정렬 : ";
-  sorttext.textContent = ascbtn.textContent;
+  sorttext.textContent = filter.sort === 0 ? "오름차순" : "내림차순";
   list.append(sort);
   sort.append(sortText);
   sortText.append(sorttext);
@@ -21,8 +21,8 @@ function createFilterBadge() {
   const datetext = document.createElement("span");
   if (filter.date === 0) date.className = "hidden";
   dateText.textContent = "기간:";
-  datetext.textContent = btntext[0].textContent;
-
+  datetext.textContent =
+    filter.date === 0 ? "전체" : filter.date === 1 ? "오늘" : "최근 7일";
   list.append(date);
   date.append(dateText);
   dateText.append(datetext);
@@ -33,7 +33,14 @@ function createFilterBadge() {
   const prioritytext = document.createElement("span");
   if (filter.priority === 0) priority.className = "hidden";
   priorityText.textContent = "우선순위:";
-  prioritytext.textContent = btntext[1].textContent;
+  prioritytext.textContent =
+    filter.priority === 0
+      ? "전체"
+      : filter.priority === 1
+        ? "높음"
+        : filter.priority === 2
+          ? "중간"
+          : "낮음";
   list.append(priority);
   priority.append(priorityText);
   priorityText.append(prioritytext);
--- js/filter.js
+++ js/filter.js
@@ -8,4 +8,27 @@ function filter() {
       sensitivity: "base",
     }),
   );
+
+  const priority = {
+    0: "전체기간",
+    1: "high",
+    2: "medium",
+    3: "low",
+  };
+
+  const priorityfilter = [...todos].filter(
+    (todo) => todo.priority === priority[filter.priority],
+  );
 }
+
+function period(todos) {
+  const now = Date.now();
+  const in7days = todos.filter((todo) => {
+    const diff = now - todo.createAt;
+    const diffDays = diff / (1000 * 60 * 60 * 24);
+    return diffDays <= 7;
+  });
+  return in7days;
+}
+
+function Istoday(todos) {}
