0ab5f0f018c12f9a07f87da6d92658d8274d0632
OwO1802 <aktld101@naver.com>
2026-01-28 09:20:59 +0900

    필터 로직 수정

--- js/filter.js
+++ js/filter.js
@@ -1,55 +1,56 @@
 function filterText() {
-  const filter = getStorage(FLOWDASH_FILTER);
-  datebtntext.textContent = periodObj[filter.date];
-  prioritybtntext.textContent = priorityObj[filter.priority];
-  ascbtn.textContent = sortObj[filter.sort];
+  const filter = getStorage(FLOWDASH_FILTER)
+  datebtntext.textContent = periodObj[filter.date]
+  prioritybtntext.textContent = priorityObj[filter.priority]
+  ascbtn.textContent = sortObj[filter.sort]
 }
 
 function sortTodos(todos, filter) {
   todos.sort((a, b) => {
     return filter.sort
-      ? b.title.localeCompare(a.title, "ko", {
+      ? b.title.localeCompare(a.title, 'ko', {
           numeric: true,
-          sensitivity: "base",
+          sensitivity: 'base',
         })
-      : a.title.localeCompare(b.title, "ko", {
+      : a.title.localeCompare(b.title, 'ko', {
           numeric: true,
-          sensitivity: "base",
-        });
-  });
+          sensitivity: 'base',
+        })
+  })
 }
 
 function getFilteredTodos(todos, filter) {
+  const keyWord = getStorage(FLOWDASH_SEARCH).toLowerCase().trim()
   return todos.filter((todo) => {
-    const isWithinDate = filter.date === 0 || checkDate(todo.id, filter.date);
-
     function checkDate(id, date) {
-      const now = Date.now();
-      const oneDay = 24 * 60 * 60 * 1000;
-      const today = new Date().setHours(0, 0, 0, 0);
+      const now = Date.now()
+      const oneDay = 24 * 60 * 60 * 1000
+      const today = new Date().setHours(0, 0, 0, 0)
 
-      return date == 1 ? id >= today : now - id <= oneDay * 7;
+      return date == 1 ? id >= today : now - id <= oneDay * 7
     }
 
-    const matchesPriority =
-      filter.priority === 0 ||
-      todo.priority === priorityfilterObj[filter.priority];
+    const isWithinDate = filter.date === 0 || checkDate(todo.id, filter.date)
 
-    return isWithinDate && matchesPriority;
-  });
+    const matchesPriority =
+      filter.priority === 0 || todo.priority === priorityfilterObj[filter.priority]
+
+    const matchesSearch =
+      todo.title.toLowerCase().includes(keyWord) || todo.content.toLowerCase().includes(keyWord)
+
+    return isWithinDate && matchesPriority && matchesSearch
+  })
 }
 
 function filterListNumbers(filteredList) {
-  const todolistnum = document.querySelector(".list-todo >.todo-items");
-  const doinglistnum = document.querySelector(".list-doing > .doing-items");
-  const donelistnum = document.querySelector(".list-done > .done-items");
+  const todolistnum = document.querySelector('.list-todo >.items-count')
+  const doinglistnum = document.querySelector('.list-doing > .items-count')
+  const donelistnum = document.querySelector('.list-done > .items-count')
 
-  const todoCount = filteredList.filter((t) => t.statusvalue === "todo").length;
-  const doingCount = filteredList.filter(
-    (t) => t.statusvalue === "doing",
-  ).length;
-  const doneCount = filteredList.filter((t) => t.statusvalue === "done").length;
-  todolistnum.textContent = todoCount;
-  doinglistnum.textContent = doingCount;
-  donelistnum.textContent = doneCount;
+  const todoCount = filteredList.filter((t) => t.statusvalue === 'todo').length
+  const doingCount = filteredList.filter((t) => t.statusvalue === 'doing').length
+  const doneCount = filteredList.filter((t) => t.statusvalue === 'done').length
+  todolistnum.textContent = todoCount
+  doinglistnum.textContent = doingCount
+  donelistnum.textContent = doneCount
 }
--- js/search.js
+++ js/search.js
@@ -1,91 +1,75 @@
 //검색어 필터 제거
 function removeFilter() {
-  const removeSearch = sortList.querySelector('.search');
+  const removeSearch = sortList.querySelector('.search')
   if (removeSearch) {
-    removeSearch.remove();
-    localStorage.removeItem(FLOWDASH_SEARCH);
+    removeSearch.remove()
+    localStorage.removeItem(FLOWDASH_SEARCH)
   }
 }
 
 //배지 생성
 function serachBadge(text) {
-  const search = document.createElement('li');
-  search.className = 'sort-list-badge search';
-  const searchTitle = document.createElement('p');
-  const searchFilter = document.createElement('span');
-  searchTitle.textContent = '검색어 : ';
-  searchFilter.textContent = text;
+  const search = document.createElement('li')
+  search.className = 'sort-list-badge search'
+  const searchTitle = document.createElement('p')
+  const searchFilter = document.createElement('span')
+  searchTitle.textContent = '검색어 : '
+  searchFilter.textContent = text
   //필터가 이미 있을 경우 기존 필터 삭제
-  removeFilter();
+  removeFilter()
   //요소 추가
-  sortList.appendChild(search);
-  search.appendChild(searchTitle);
-  searchTitle.appendChild(searchFilter);
-}
-
-//검색어 필터 함수, 애니메이션까진 아직 적용 X
-function sortSearch() {
-  const keyWord = getStorage(FLOWDASH_SEARCH).toLowerCase().trim();
-  const elementCard = document.querySelectorAll('.todo-element');
-
-  elementCard.forEach((card) => {
-    const title = card.querySelector('.element-title').textContent.toLowerCase().trim();
-    const desc = card.querySelector('.element-description').textContent.toLowerCase().trim();
-    if (title.includes(keyWord) || desc.includes(keyWord)) {
-      card.classList.remove('hidden');
-    } else {
-      card.classList.add('hidden');
-    }
-  });
+  sortList.appendChild(search)
+  search.appendChild(searchTitle)
+  searchTitle.appendChild(searchFilter)
 }
 
 //실시간 입력 디바운스 함수
 function debounce(func, delay) {
-  let timer;
+  let timer
   //실핼될 익명 함수 반환 -> 들어오는 모든 인자를 배열로 모으기(Rest)
   return function (...args) {
     //새로 입력하면 이전 입력에 적용된 타이머 취소
-    if (timer) clearTimeout(timer);
+    if (timer) clearTimeout(timer)
     //입력을 멈추면 딜레이 후 개별 인자로 다시 전달(Spread)
     timer = setTimeout(() => {
-      func(...args);
-    }, delay);
-  };
+      func(...args)
+    }, delay)
+  }
 }
 
 function search(str) {
   if (!str) {
-    removeFilter();
+    removeFilter()
   } else {
-    setStorage(FLOWDASH_SEARCH, str);
+    setStorage(FLOWDASH_SEARCH, str)
   }
-  render();
+  render()
 }
+
+//폼 제출 방지
+form.addEventListener('submit', (e) => e.preventDefault())
 
 //입력시 실시간 필터 적용
 searchForm.addEventListener(
   'input',
   //입력값을 e 매개변수를 통해 인자로 받아옴
   debounce(function (e) {
-    const text = e.target.value.toLowerCase().trim(); //받아온 인자를 개별로 다시 전달
-    search(text);
+    const text = e.target.value.toLowerCase().trim() //받아온 인자를 개별로 다시 전달
+    search(text)
   }, 300)
-);
-
-//폼 제출 방지
-form.addEventListener('submit', (e) => e.preventDefault());
+)
 
 //엔터 혹은 ESC 입력시 딜레이 없이 바로 필터 적용
 searchForm.addEventListener('keydown', (e) => {
-  const text = e.target.value.toLowerCase().trim();
+  const text = e.target.value.toLowerCase().trim()
   if (e.key === 'Enter' || e.key === 'Escape') {
-    e.preventDefault();
-    search(text);
+    e.preventDefault()
+    search(text)
   }
-});
+})
 
 //검색 아이콘 클릭시 딜레이 없이 필터 적용
 searchIcon.addEventListener('click', () => {
-  const text = searchForm.value.toLowerCase().trim();
-  search(text);
-});
+  const text = searchForm.value.toLowerCase().trim()
+  search(text)
+})
