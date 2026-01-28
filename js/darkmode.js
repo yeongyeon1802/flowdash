3b6b3ea07b8de906e502a37c546debb01312ea09
OwO1802 <aktld101@naver.com>
2026-01-28 15:16:18 +0900

    feat: 다크모드 버튼 스위치방식으로 업데이트

--- js/darkmode.js
+++ js/darkmode.js
@@ -12,11 +12,18 @@ function allToggleDarkMode(element, darkModeClass, boolean) {
 
 //다크모드 상태 로컬스토리지 저장
 let darkMode = getStorage(FLOWDASH_THEME);
-
+const darkModeToggleBtn = document.querySelector(".active");
 //다크모크 토글 이벤트
 function darkModeUI() {
   const isDark = darkMode === "dark";
 
+  const todoElementDarkMode = document.querySelectorAll(".todo-element");
+  const elementTitleDarkMode = document.querySelectorAll(".element-title");
+  const elementDescDarkMode = document.querySelectorAll(".element-description");
+  const elementDeleteBtn = document.querySelectorAll(".card-top > button");
+  const elementDeleteIcon = document.querySelectorAll(".bi-x-lg");
+  const elementHoverShadow = document.querySelectorAll(".element-list");
+  const doneElementBorderDarkMode = document.querySelectorAll(".element-done");
   //querySelector 요소
   toggleDarkMode(document.body, "body-darkmode", isDark);
   toggleDarkMode(footerBorderDarkMode, "todo-footer-darkmode", isDark);
@@ -25,6 +32,9 @@ function darkModeUI() {
   toggleDarkMode(popupTitleDarkMode, "popup-content-title-darkmode", isDark);
   toggleDarkMode(popupDescDarkMode, "popup-description-darkmode", isDark);
   toggleDarkMode(cancelbtn, "popup-cancel-button-darkmode", isDark);
+  toggleDarkMode(formDarkMode, "form-container-darkmode", isDark);
+  toggleDarkMode(darkModeToggleBtn, "active-darkmode", isDark);
+  toggleDarkMode(darkModeBtn, "theme-btn-darkmode", isDark);
   //querySelectorAll 요소
   allToggleDarkMode(elementDarkMode, "big-element-darkmode", isDark);
   allToggleDarkMode(bigTextDarkMode, "big-text-darkmode", isDark);
@@ -35,13 +45,21 @@ function darkModeUI() {
   allToggleDarkMode(listDoneLineDarkMode, "list-done-darkmode", isDark);
   allToggleDarkMode(priorityItemDarkMode, "priority-item-darkmode", isDark);
   allToggleDarkMode(statusItemDarkMode, "status-item-darkmode", isDark);
+  allToggleDarkMode(todoElementDarkMode, "todo-element-darkmode", isDark);
+  allToggleDarkMode(elementTitleDarkMode, "element-title-darkmode", isDark);
+  allToggleDarkMode(elementDescDarkMode, "element-description-darkmode", isDark);
+  allToggleDarkMode(elementDeleteBtn, "element-delete-button-darkmode", isDark);
+  allToggleDarkMode(elementDeleteIcon, "b1-x-1g-darkmode", isDark);
+  allToggleDarkMode(elementHoverShadow, "element-list-darkmode", isDark);
+  allToggleDarkMode(doneElementBorderDarkMode, "element-done-darkmode", isDark);
+  allToggleDarkMode(listFilterIcon, "bi-funnel-fill-darkmode", isDark);
 
   if (darkMode === "dark") {
-    brightIcon.classList.add("bright-icon-darkmode");
-    darkIcon.classList.add("dark-icon-darkmode");
-  } else {
     brightIcon.classList.remove("bright-icon-darkmode");
     darkIcon.classList.remove("dark-icon-darkmode");
+  } else {
+    brightIcon.classList.add("bright-icon-darkmode");
+    darkIcon.classList.add("dark-icon-darkmode");
   }
 }
 
